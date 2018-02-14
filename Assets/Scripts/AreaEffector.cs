af2653edb1bb3dd0ee76921476090a23bcf8d758
David Soto <urielsonic@gmail.com>
2018-02-14 08:53:16 -0600

    Se suben los cambios de la segunda escena del Laboratorio 5

--- Assets/Scripts/AreaEffector.cs
+++ Assets/Scripts/AreaEffector.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//David Uriel Soto Alvarez 
+//17551
+
 public class AreaEffector : MonoBehaviour {
     public float time;
-    public GameObject child;
-    public GameObject child2;
-    bool active = true;
+    public GameObject child; //Declara un Objeto 
+    public GameObject child2; //Declara otro Objeto
+    bool active = true; //Variable que nos ayudara a activar y desactivar los objetos
 	// Use this for initialization
 	void Start () {
 		
@@ -14,13 +17,13 @@ public class AreaEffector : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        time = time + Time.deltaTime;
+        time = time + Time.deltaTime; //Ayudara a medir el tiempo
         if(time >= 3f)
         {
-            active = !active;
-            child.SetActive(active);
-            child2.SetActive(active);
-            time = 0;
+            active = !active; //Cambiara el estado de la variable
+            child.SetActive(active); //Activa o desactiva el objeto
+            child2.SetActive(active); //Activa o desactiva el objeto
+            time = 0; //Reinicia el tiempo
         }
 	}
 }
---
+++ Assets/Scripts/Weight.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//David Uriel Soto Alvarez 
+//17551
+
+public class Weight : MonoBehaviour {
+    LineRenderer line; //Crea un objeto de tipo Line Renderer
+    DistanceJoint2D distanceJoint; //Crea un objeto de tipo distance joint
+
+	// Use this for initialization
+	void Start () {
+        distanceJoint = GetComponent<DistanceJoint2D>(); //Obtiene las variables del programa
+        line = GetComponent<LineRenderer>(); //Obtiene las variables del objeto
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        line.SetPosition(0, transform.position); //Actualiza la posicion de inicio de la linea renderer
+        line.SetPosition(1,distanceJoint.connectedBody.transform.position); //Actualiza la posicion del final de la linea renderer
+    }
+}
